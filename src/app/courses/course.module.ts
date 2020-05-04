73c895ae1211e017aad967145cba03c3bf4a5f0b
LeticiaLCavalcanti <wmli515@gmail.coma>
2020-05-04 03:17:37 -0300

    creation of shared directory and division of modules

--- src/app/courses/course.module.ts
+++ src/app/courses/course.module.ts
@@ -3,20 +3,20 @@ import {CourseListComponent} from './course-list.component';
 import {CourseInfoComponent} from './course-info.component';
 import { RouterModule } from '@angular/router';
 import {FormsModule} from "@angular/forms";
-import {ReplacePipe} from "../pipe/replace.pipe";
 import {CommonModule} from "@angular/common";
-import {StarComponent} from "../star/star.component";
+import {StarModule} from "../shared/component/star/star.module";
+import {AppPipeModule} from "../shared/pipe/app-pipe.module";
 
 @NgModule ({
     declarations: [
         CourseListComponent,
         CourseInfoComponent,
-        ReplacePipe,
-        StarComponent
     ],
     imports: [
         CommonModule,
         FormsModule,
+        StarModule,
+        AppPipeModule,
         RouterModule.forChild([
             {
                 path: 'courses',
